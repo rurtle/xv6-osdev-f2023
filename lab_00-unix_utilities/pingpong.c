a4e213496c4b9cce6a79559abc5574d26fcde45f
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2025-08-07 18:55:41 +0530

    [Lab-00] Finally, a working version of the pingpong system call

--- lab_00-unix_utilities/pingpong.c
+++ lab_00-unix_utilities/pingpong.c
@@ -6,14 +6,13 @@ int
 main(int argc, char *argv[])
 {
 	int pipefd[2];
+	char c;
 	int cpid;
-	char c = 'x';
 
 	if (argc != 1) {
 		fprintf(2, "Usage: pingpong\n");
 		exit(1);
 	}
-
 	if ((pipe(pipefd)) == -1) {
 		fprintf(2, "pipe failure\n");
 		exit(1);
@@ -26,20 +25,31 @@ main(int argc, char *argv[])
 	}
 
 	if (cpid == 0) {	/* Child reads from the pipe */
-
+		close(pipefd[0]);
 		// Print the <pid>: received ping
+		printf("%d: received ping\n", getpid());
 
 		// Write a byte
+		write(pipefd[1], "x", 1);
+		close(pipefd[1]);
 
 		// exit
+		exit(0);
 	} else {
+		// Wait for the child process to complete
+		wait(0);
 		// Send a byte to the child
+		write(pipefd[1], "x", 1);
 
 		// Read the byte from the child
+		read(pipefd[0], &c, 1);
 
 		// Print the <pid>: received pong
+		printf("%d: received pong\n", getpid());
 
-		// exit
+		// close file handles
+		close(pipefd[0]);
+		close(pipefd[1]);
 	}
 
 	exit(0);
